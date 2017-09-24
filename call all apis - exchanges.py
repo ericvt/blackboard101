50510c60b925155d5b0eaf3723a232f302e6eed6
Eric Van Thorre <ericvt@users.noreply.github.com>
2017-09-24 07:39:31 -0700

    Update call all apis - exchanges.py
    btc-e exchange has been taken down, removed reference

--- call all apis - exchanges.py
+++ call all apis - exchanges.py
@@ -20,14 +20,6 @@ def btstamp():
     bitStampTick = requests.get('https://www.bitstamp.net/api/ticker/')
     return bitStampTick.json()['last'] # replace last with timestamp etc
 
-def btceBU():
-    btceBtcTick = requests.get('https://btc-e.com/api/2/btc_usd/ticker')
-    return btceBtcTick.json()['ticker']['last'] # replace last with updated etc
-
-def btceBL():
-    btceLtcTick = requests.get('https://btc-e.com/api/2/ltc_btc/ticker')
-    return btceLtcTick.json()['ticker']['last'] # replace last with updated etc
-
 def bitfinex(): 
     bitFinexTick = requests.get("https://api.bitfinex.com/v1/ticker/btcusd")
     return bitFinexTick.json()['last_price']
@@ -43,22 +35,17 @@ def kraken():
 
 while True:
     btstampUSDLive = float(btstamp())
-    btceUSDLive = float(btceBU())
-    btceLTCinBTCLive = float(btceBL())
     coinbUSDLive = float(coinbase())
     krakenUSDLive = float(kraken())
     bitfinexUSDLive = float(bitfinex())
 
     print "Bitstamp Price in USD =", btstampUSDLive
-    print "BTC-e Price in USD =", btceUSDLive
     print "Coinbase Price in USD =", coinbUSDLive
     print "Kraken Price in USD =", krakenUSDLive
     print "Bitfinex Price in USD =", bitfinexUSDLive
     print "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-="
     print "BTC USD Average = ", 
-    print (krakenUSDLive + coinbUSDLive + btceUSDLive + btstampUSDLive + bitfinexUSDLive) / 5
+    print (krakenUSDLive + coinbUSDLive + btstampUSDLive + bitfinexUSDLive) / 5
     print "difference betweeen coinbase and bitstamp", (coinbUSDLive - btstampUSDLive)
-    print "BTC-e LTC Price in BTC", btceLTCinBTCLive
-    print "BTC-e LTC Price in USD =", btceLTCinBTCLive * btceUSDLive, "USD"
     print; print; print "=-=-=-=-=-=-=--=-=-=-"; print;
     time.sleep(2) # 120 equals two minutes, you can ping it every second by putting a 1 in here
